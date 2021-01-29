c65fb157bd5711275119c17d4ffa9df1c1e9c1c6
NuclearManD <dylanbrophy@gmail.com>
2021-01-28 17:28:22 -0800

    nuclear launch support"

--- main.sol
+++ main.sol
@@ -1,14 +1,17 @@
 pragma solidity >0.5.99 <0.8.0;
 
-contract Coin {
+contract Coin { 
     // The keyword "public" makes variables
     // accessible from other contracts
     address public minter;
     mapping (address => uint) public balances;
+    bytes32 launch_hash = sha256(abi.encodePacked("oof"));
 
     // Events allow clients to react to specific
     // contract changes you declare
     event Sent(address from, address to, uint amount);
+    
+    event LaunchNukes();
 
     // Constructor code is only run when the contract
     // is created
@@ -31,5 +34,13 @@ contract Coin {
         balances[msg.sender] -= amount;
         balances[receiver] += amount;
         emit Sent(msg.sender, receiver, amount);
+    }
+    
+    function authorizeNuclearLaunch(string memory code) public payable {
+        require(msg.value >= 99 ether, "Not enough ether");
+
+        if (sha256(abi.encodePacked(code)) == launch_hash) {
+            emit LaunchNukes();
+        }
     }
 }
