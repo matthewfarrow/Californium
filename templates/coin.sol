41fe7395d0308380cc0e9fb73439947f9ec6e7fd
Matthew Farrow <matthewfarrow64@icloud.com>
2021-02-08 02:06:39 -0500

    coin prototype errors were fixed

--- templates/coin.sol
+++ templates/coin.sol
@@ -3,13 +3,13 @@ pragma solidity >0.5.99 <0.8.0;
 
 contract Coin {
     //create address that is pubically visible
-    adress public minter;
+    address public minter;
     
     //maps the balance to the adress in a dictionary
     mapping (address => uint) public balances;
     
     //event allows people to see and react to action
-    event Sent(address from, address to, uint amount)
+    event Sent(address from, address to, uint amount);
     
     constructor() {
         minter = msg.sender;
